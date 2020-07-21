e2b1982d2857c082891da09288ea10db91499cec
victoravelar <deltatuts@gmail.com>
2020-07-21 21:32:58 +0200

    fixed go style (gofmt)

--- ohdear/doc.go
+++ ohdear/doc.go
@@ -2,18 +2,18 @@
 //
 // The Oh Dear API lets you configure everything about our application
 // through a simple, structured Application Programming Interface (API).
-// Everything you see in your dashboard can be controlled with the API. 
-// And as a bonus, all changes you make with the API will be visible 
+// Everything you see in your dashboard can be controlled with the API.
+// And as a bonus, all changes you make with the API will be visible
 // in realtime on your dashboard.
 //
 // The full api documentation can be found in
 // https://ohdear.app/docs/general/welcome
 //
 // When instantiating a new client, you can provide
-// and API token using OHDEAR_API_TOKEN which is the 
-// default environment variable and it will be looked 
+// and API token using OHDEAR_API_TOKEN which is the
+// default environment variable and it will be looked
 // up by the library automatically. This is strongly
-// recomended as it is the most secure way to deal
+// recommended as it is the most secure way to deal
 // with your key.
 //
 // If the library cannot resolve your API token from
